5a014b3c88054f1b4fed0914e7856d7614e428af
Kevin Pierce <piercekg@gmail.com>
2021-02-26 14:43:50 -0700

    added axios to client/requests.js for making reqs, added server as const for making reqs, added getRelatedProducts req

--- client/requests.js
+++ client/requests.js
@@ -1,6 +1,21 @@
+const axios = require('axios');
+
+const server = 'http://localhost:3000';
+
 // Requests go here
 
 // PRODUCTS REQUESTS
+
+// RELATED PRODUCTS REQUESTS
+const getRelatedProducts = (productId, callback) => {
+  axios.get(`${server}/products/${productId}/related`)
+    .then((data) => {
+      callback(null, data);
+    })
+    .catch((err) => {
+      callback(err, null);
+    })
+}
 
 // REVIEWS REQUESTS
 
