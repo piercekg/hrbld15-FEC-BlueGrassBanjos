cd4a1dc8b34ad9b8dddf945d41ec8e2f587733ff
Kevin Pierce <piercekg@gmail.com>
2021-02-26 14:33:49 -0700

    functionality for handling req from client, sending req to API, and sending data back to client

--- server/index.js
+++ server/index.js
@@ -6,12 +6,23 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-const server = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld'
+const API = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld'
 
 // PRODUCTS REQUESTS
 
 // RELATED PRODUCTS REQUESTS
-const getRelatedProducts = (productId, callback) => {
+app.get('/products/:product_id/related', (req, res) => {
+  retrieveRelatedProducts(req.params.id, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      res.status(200).send(data);
+    }
+  })
+});
+
+const retrieveRelatedProducts = (productId, callback) => {
   axios.get(`${server}/products/${productId}/related`)
     .then((data) => {
       callback(null, data);
