a61bcbd3e969acb8e7fc7e0f50f93a9a211d5b07
Kevin Pierce <piercekg@gmail.com>
2021-02-26 14:16:48 -0700

    added axios to server/index.js to make reqs, added server as const for reqs, wrote getRelatedProducts GET req

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
 const path = require('path');
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
+const server = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld'
+
 // PRODUCTS REQUESTS
+
+// RELATED PRODUCTS REQUESTS
+const getRelatedProducts = (productId, callback) => {
+  app.get(server + `/products/${productId}/related`)
+    .then((data) => {
+      callback(null, data);
+    })
+    .catch((err) => {
+      callback(err, null);
+    })
+};
 
 // REVIEWS REQUESTS
 
