f9a9462341ba33fa9d51d51788627196eed709cf
David Goelitz <davidgoelitz@tests-MacBook-Pro.local>
2021-03-01 12:39:43 -0700

    Able to fetch data, both product and style, painting basic product info to the screen. Now moving on to testing

--- client/App.jsx
+++ client/App.jsx
@@ -3,20 +3,23 @@ import {
   // eslint-disable-next-line no-unused-vars
   BrowserRouter as Router, Switch, Route, Link,
 } from 'react-router-dom';
+import Overview from './productOverview/displayOverview';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      product: 18201,
     };
   }
 
   render() {
+    const prod = this.state;
     return (
       <Router>
         <div className="hello">Hello World!!!!</div>
+        <Overview product={prod.product} />
       </Router>
     );
   }
---
+++ client/productOverview/displayOverview.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const requests = require('../requests.js');
+
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: {},
+      style: {},
+    };
+    this.getProduct(props);
+    this.getStyles(props);
+  }
+
+  getProduct(props) {
+    requests.getProductInfo(props.product, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        this.setState({
+          product: data.data,
+        });
+      }
+    });
+  }
+
+  getStyles(props) {
+    requests.getProductStyles(props.product, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        this.setState({
+          style: data.data.results,
+        });
+      }
+    });
+  }
+
+  render() {
+    this.prod = this.state;
+    this.style = this.prod.style;
+    this.prod = this.prod.product;
+    console.log(this.style);
+    return (
+      <div>
+        <h1>{this.prod.name}</h1>
+        <h5>{this.prod.default_price}</h5>
+        <h3>{this.prod.category}</h3>
+      </div>
+    );
+  }
+}
+
+export default Overview;
