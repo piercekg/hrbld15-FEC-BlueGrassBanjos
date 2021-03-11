9ce6912104314d121037f748a744e9b46e806586
David Goelitz <davidgoelitz@tests-MacBook-Pro.local>
2021-03-11 15:16:55 -0700

    Minor changes to merge

---
+++ client/components/productOverview/displayOverview.jsx
@@ -0,0 +1,67 @@
+/* eslint-disable react/no-unused-state */
+import React from 'react';
+import Gallery from './imagesGallery';
+
+const requests = require('../../requests.js');
+
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null,
+      style: null,
+    };
+    this.getProduct(props);
+    this.getStyles(props);
+  }
+
+  getProduct(props) {
+    requests.getProductInfo(props.product, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        this.setState({
+          product: data.data,
+        });
+      }
+    });
+  }
+
+  getStyles(props) {
+    requests.getProductStyles(props.product, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        this.setState({
+          style: data.data.results,
+        });
+      }
+    });
+  }
+
+  render() {
+    this.prod = this.state;
+    if (!this.prod.style || !this.prod.product) {
+      return <div />;
+    }
+    this.style = this.prod.style;
+    this.product = this.prod.product;
+    // console.log('should be url: ', this.style[0]);
+    return (
+      <div>
+        <h1>
+          <Gallery
+            image={this.style[0].photos[0].url}
+            product={this.product.name}
+            style={this.style[0].name}
+          />
+        </h1>
+        <h1>{this.product.name}</h1>
+        <h5>{this.product.default_price}</h5>
+        <h3>{this.product.category}</h3>
+      </div>
+    );
+  }
+}
+
+export default Overview;
